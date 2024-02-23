a39cdf5eef98ddfe33495ad5af4630736aab3904
Maxi <dorogovmax@yandex.ru>
2024-02-23 19:48:12 +0300

    Задачу спринта №9

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,99 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.view.doOnLayout
 
 class SearchActivity : AppCompatActivity() {
+    var inputSearchText: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            val backButtonIntent = Intent(this, MainActivity::class.java)
+            startActivity(backButtonIntent)
+        }
+
+        val inputEditText = findViewById<EditText>(R.id.search_edit_text)
+        val deleteButton = findViewById<ImageView>(R.id.delete_button)
+        inputEditText.doOnLayout { inputEditText.hint }
+
+
+
+        deleteButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+        if (inputSearchText != null) {
+            inputEditText.setText(inputSearchText)
+        }
+
+
+        val simpleTextWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null) {
+                    if (s.isNullOrEmpty()) {
+                        deleteButton.visibility = View.GONE
+                        val inputMethodManager =
+                            getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                        inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+                    } else {
+                        deleteButton.visibility = View.VISIBLE
+                        inputEditText.requestFocus()
+                        inputSearchText = s.toString()
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //
+            }
+
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(inputSearchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputSearchText = savedInstanceState.getString(inputSearchText)
+
+    }
+
+
+    private fun deleteButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+
 }
+
+private fun Bundle.putString(inputSearchText: String?) {
+
+}
+
+
+
+
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
@@ -16,7 +18,50 @@ val backButtonIntent = Intent(this, MainActivity::class.java)
             startActivity(backButtonIntent)
         }
 
+        val shareButton = findViewById<FrameLayout>(R.id.share_button)
+        shareButton.setOnClickListener{
+            shareApp()
+        }
+
+        val supportButton = findViewById<FrameLayout>(R.id.support_button)
+        supportButton.setOnClickListener{
+            sendEmail()
+        }
+
+        val agreementButton = findViewById<FrameLayout>(R.id.agreement_button)
+        agreementButton.setOnClickListener{
+            showAgreement()
+        }
 
 
+
+
+    }
+
+    private fun showAgreement() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.agreement_link)))
+        startActivity(intent)
+    }
+
+    private fun shareApp() {
+        val shareButtonIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_link))
+            type = "text/html"
+        }
+        val intent = Intent.createChooser(shareButtonIntent, null)
+        startActivity(intent)
+    }
+
+    private fun sendEmail() {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, getString(R.string.test_email))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject_support_mail) +
+                    " Playlist Maker")
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.text_support_mail))
+
+        }
+            startActivity(intent)
     }
 }
